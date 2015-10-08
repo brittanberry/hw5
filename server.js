f0db1872d45c62ba9ae36bad5e432ffeb9868e25
brittanberry <brittan.berry@yale.edu>
2015-10-08 23:40:45 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Woohoo! I am a coding ninja noW!');
 });
+
+app.get('/nickname', function(request, response){
+    response.send('outstanding-lark');
+});
+
+
 
 app.listen(process.env.PORT || 4000);
